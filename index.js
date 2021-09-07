ab510aa3a899d4c7452e431f516339b4f62cca4e
mahak <makhariamahak@gmail.com>
2021-09-07 12:33:40 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,133 @@
+var readlineSync = require('readline-sync');
+const chalk = require('chalk');
+
+var score = 0;
+
+const questions = [
+    {
+        question: "What street do Holmes and Watson live on? ",
+        answer: "Baker Street"
+    },
+    {
+        question: "What is Watson's official job ",
+        answer: "doctor"
+    },
+    {
+        question: "What is the name of Holmes' landlord? ",
+        answer: "Mrs Hudson"
+    },
+    {
+        question: "What is the name of Holmes' older brother? ",
+        answer: "Mycroft"
+    },
+    {
+        question: "What is the name of Sherlock Holmes' arch enemy? ",
+        answer: "Morairty"
+    },
+    {
+        question: "What is the name of Holmes' sister? ",
+        answer: "Eurus"
+    },
+    {
+        question: "Where does Holmes store his tobacco? ",
+        answer: "socks"
+    },
+    {
+        question: "Who were the four in 'The Sign of the Four?' ",
+        answer: "convicts"
+    },
+    {
+        question: "Who are the Baker Street Irregulars? ",
+        answer: "street urchins"
+    }
+
+];
+
+const highscores = [
+    {
+        name: "Rashi",
+        score: 8
+    },
+    {
+        name: "Mohini",
+        score: 6
+    },
+    {
+        name: "Divya",
+        score: 5
+    }
+];
+
+var userName = readlineSync.question("What's your name? ");
+
+console.log(chalk.yellow("\nHello " + userName + "!") + " Welcome to HOW WELL DO YOU KNOW SHERLOCK HOLMES :)\n");
+
+console.log(chalk.bold.yellow("There are 3 levels\n") +
+    chalk.italic("Each having 3 questions, if you get 2 right then only you move forward\n"));
+
+
+function play(question, answer) {
+    var userAnswer = readlineSync.question(question);
+
+    if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+        console.log(chalk.green("Correct!"));
+        score++;
+    }
+    else
+        console.log(chalk.red("Wrong :( \n") + "It's " + chalk.green(answer));
+
+    console.log("Current score is ", score);
+    console.log("-----------------------")
+
+}
+
+
+
+function quiz(start, end) {
+
+    for (let i = start; i <= end; i++) {
+        play(questions[i].question, questions[i].answer);
+    }
+
+}
+
+var ready = readlineSync.keyInYN("Are you ready?");
+
+if (ready) {
+    console.log(chalk.bold.bgRed("\nLevel 1"));
+    quiz(0, 2);
+    if (score >= 2) {
+        console.log(chalk.bold.bgRed("\nLevel 2"));
+        quiz(3, 5);
+        if (score >= 4) {
+            console.log(chalk.bold.bgRed("\nLevel 3"));
+            quiz(6, 8);
+        }
+        else {
+            console.log(chalk.red("Oops you're out!"))
+        }
+    }
+    else {
+        console.log(chalk.red("Oops you're out!"))
+    }
+
+}
+
+console.log("\nYaay! You scored: " + chalk.green(score));
+
+checkHighScore();
+
+function checkHighScore() {
+    for (let i = 0; i < highscores.length; i++) {
+        if (score > highscores[i].score || score == 9) {
+            console.log(chalk.bgCyan("\nCongratulations! You are a high scorer!\n") + chalk.italic("(DM me the screenshot of your score, I'll add you to the high scorers' list.)"));
+            break;
+        }
+    }
+
+    console.log(chalk.blue("\nCheck out the high scores"));
+
+    for (let i = 0; i < highscores.length; i++) {
+        console.log(highscores[i].name + ": " + chalk.bold.blue(highscores[i].score));
+    }
+}
